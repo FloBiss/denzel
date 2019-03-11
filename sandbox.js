2e88db4c9b23d1ed4d91ae3498c277712b9d3a54
FloBiss <flobissogillot@gmail.com>
2019-03-11 17:16:13 +0100

    First Push denzel senpai

--- sandbox.js
+++ sandbox.js
@@ -1,18 +1,21 @@
+
 /* eslint-disable no-console, no-process-exit */
 const imdb = require('./src/imdb');
 const DENZEL_IMDB_ID = 'nm0000243';
+const MongoClient=require('mongodb').MongoClient;
+const uri = 'mongodb+srv://FlorentBiss:Floby.hokcey1002@cluster0-nitmr.mongodb.net/test?retryWrites=true'
+
 
 async function sandbox (actor) {
   try {
-    console.log(`📽️  fetching filmography of ${actor}...`);
     const movies = await imdb(actor);
-    const awesome = movies.filter(movie => movie.metascore >= 77);
-
-    console.log(`🍿 ${movies.length} movies found.`);
-    console.log(JSON.stringify(movies, null, 2));
-    console.log(`🥇 ${awesome.length} awesome movies found.`);
-    console.log(JSON.stringify(awesome, null, 2));
-    process.exit(0);
+    const client = await MongoClient.connect(uri, {
+		useNewUrlParser : true
+	});
+    const collection = client.db("AwesomeFilms").collection("Films");
+	//perform actionon the collection object
+	collection.insertMany(movies)
+	client.close();
   } catch (e) {
     console.error(e);
     process.exit(1);
